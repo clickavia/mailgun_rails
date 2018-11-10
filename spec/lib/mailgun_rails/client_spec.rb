280ba4cd6261a176b87248c269485d83be10bad6
MartinGantenbein <martin.gantenbein@gmail.com>
2018-11-10 18:04:41 +0100

    Fix failing tests, add missing tests

--- spec/lib/mailgun_rails/client_spec.rb
+++ spec/lib/mailgun_rails/client_spec.rb
@@ -2,13 +2,27 @@ require 'spec_helper'
 require 'mailgun_rails/client'
 
 describe MailgunRails::Client do
-  let(:client){MailgunRails::Client.new(:some_api_key, :some_domain)}
+  describe "MailgunRails client without a region" do
+    let(:client){MailgunRails::Client.new(:some_api_key, :some_domain)}
 
-  describe "#send_message" do
-    it 'should make a POST rest request passing the parameters to the mailgun end point' do
-      expected_url = "https://api:some_api_key@api.mailgun.net/v3/some_domain/messages"
-      RestClient::Request.stub(:execute).with({ method: :post, url: expected_url, payload: { foo: :bar }, verify_ssl: true})
-      client.send_message foo: :bar
+    describe "#send_message" do
+      it 'should make a POST rest request passing the parameters to the mailgun end point' do
+        expected_url = "https://api:some_api_key@api.mailgun.net/v3/some_domain/messages"
+        RestClient::Request.stub(:execute).with({ method: :post, url: expected_url, payload: { foo: :bar }, verify_ssl: true})
+        client.send_message foo: :bar
+      end
     end
+  end
+
+  describe "MailgunRails client with some region" do
+    let(:client){MailgunRails::Client.new(:some_api_key, :some_domain, true, :some_region)}
+
+    describe "#send_message" do
+      it 'should make a POST rest request passing the parameters to the mailgun end point' do
+        expected_url = "https://api:some_api_key@api.some_region.mailgun.net/v3/some_domain/messages"
+        RestClient::Request.stub(:execute).with({ method: :post, url: expected_url, payload: { foo: :bar }, verify_ssl: true})
+        client.send_message foo: :bar
+      end
+    end  
   end
 end
